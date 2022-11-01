f2b6c5186aec629303cf0291b719e43b817d6bdc
Facundo Perez Montalvo <facundoperezmontalvo@gmail.com>
2022-11-01 11:25:11 -0300

    first commit

---
+++ backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+  ],
+});
+
+schema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model("User", schema);
+
+module.exports = User;
